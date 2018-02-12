0c5092f8586e44174e29615eacb666eeceb0bd5b
Joseph Chen <joseph.chen@gmail.com>
2018-02-12 13:39:29 -0800

    Add UMD definition

--- src/sonic.js
+++ src/sonic.js
@@ -4,7 +4,22 @@
  * https://github.com/padolsey/Sonic
  */
 
-(function(){
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+		define([], function () {
+			return (root.Sonic = factory());
+		});
+    } else if (typeof module === 'object' && module.exports) {
+		// Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.Sonic = factory();
+    }
+}(this, function () {
 
 	var emptyFn = function(){};
 
@@ -354,7 +369,7 @@
 
 		}
 	};
-
-	window.Sonic = Sonic;
-
-}());
+	
+    //    exposed public method
+    return Sonic;
+}));
